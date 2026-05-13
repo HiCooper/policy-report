ce86848f054cfae8647a500fd6c68358e1348b8d
卡卡西 <294237781@qq.com>
2026-05-13 12:55:04 +0800

    x

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/policy-report/', // 必须与部署子路径完全一致
   plugins: [react(), tailwindcss()],
 })
